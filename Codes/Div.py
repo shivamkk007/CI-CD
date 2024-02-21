01a67c25b1a84a62eb201a22527758dffc5ce688
Shivam KK <kshivam523@gmail.com>
2024-02-21 22:36:50 +0530

    V-1.1

--- Codes/Div.py
+++ Codes/Div.py
@@ -2,9 +2,9 @@ def Div(a,b):
     return a/b
 
 def test_div():
-    assert Div(2/2)==1
-    assert Div(4/2)==2
-    assert Div(4/1)==1
-    assert Div(0/1)==0
+    assert Div(2,2)==1
+    assert Div(4,2)==2
+    assert Div(4,1)==1
+    assert Div(0,1)==0
 
 # Test Commit
