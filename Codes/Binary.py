1521203610870f990a203c7062ce9c8a9b6ae52b
Shivam KK <kshivam523@gmail.com>
2024-02-22 13:55:56 +0530

    V-1.2

--- Codes/Binary.py
+++ Codes/Binary.py
@@ -6,7 +6,10 @@ def Binary(list,target):
         mid=(hi+lo)//2
         
         if list[mid]==target:
-            return mid
+            if list[mid-1]==target:
+                return mid-1
+            else:
+                return mid
         
         if list[mid]<target:
             lo=mid+1
@@ -19,7 +22,41 @@ def Binary(list,target):
 def test():
     
     assert Binary([1,2,4,5,6],4)==2
+    assert Binary([1,2,4,5,5,6],5)==3
+    assert Binary([1,2,4,5,6,7,8],8)==6
     
-def test_custom():
     
+# def test_custom():
     
+#     tests=[]
+
+#     tests.append({
+#         'input':{
+#             'cards':[],
+#             'query':7
+#         }, 'output':-1
+#     })
+#     tests.append({
+#         'input':{
+#             'cards':[10,6,4,2,1],
+#             'query':7
+#         }, 'output':-1
+#     })
+#     tests.append({
+#         'input':{
+#             'cards':[9,7,6,4,3,2,1],
+#             'query':7
+#         }, 'output':1
+#     })
+#     tests.append({
+#         'input':{
+#             'cards':[9,8,7,7,6,5,4,3],
+#             'query':7
+#         }, 'output':2
+#     })
+#     tests.append({
+#         'input':{
+#             'cards':[19,18,17,17,16,5,4,3],
+#             'query':4
+#         }, 'output':6
+#     })
